e478226a97f07081ff7117c43949e21a45e65398
juyoung <wnduddld@gmail.com>
2022-01-25 23:29:40 +0900

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by 김주영 on 2022/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        overrideUserInterfaceStyle = .dark
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
